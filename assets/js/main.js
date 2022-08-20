707797e1911b70cb06edc8cc8810e5a9185cf351
Ian Marshall <ian.js.tutor@gmail.com>
2022-08-20 09:50:27 -0400

    ui cursor feedback

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,16 @@
+//settings
+const uiColor = "rgba(255, 255, 0, 0.25)";
+
 //DOM elements
 const [drawingCanvas, uiCanvas] = document.querySelectorAll("canvas");
 const header = document.querySelector("header");
 const nav = document.querySelector("nav");
 const [color, penSize, eraserSize] = document.querySelectorAll("fieldset > input");
+const [eraseButton, downloadButton] = document.querySelectorAll("nav > button");
+
+//state
+var drawing = false;
+var prevPoint = null;
 
 //canvases & resizing
 const drawingCtx = drawingCanvas.getContext("2d");
@@ -23,12 +31,48 @@ document.querySelectorAll("[name='tool']").forEach(t => {
 document.addEventListener("mousedown", removeHeader);
 document.addEventListener("touchstart", removeHeader);
 document.addEventListener("pointerdown", removeHeader);
+
 function removeHeader() {
     header.classList.add("hide");
     document.removeEventListener("mousedown", removeHeader);
     document.removeEventListener("touchstart", removeHeader);
     document.removeEventListener("pointerdown", removeHeader);
 }
+
+//ui canvas
+uiCanvas.addEventListener("mousemove", moveUiCursor);
+uiCanvas.addEventListener("touchmove", moveUiCursor);
+uiCanvas.addEventListener("pointermove", moveUiCursor);
+
+function moveUiCursor(e) {
+    if (!e) {
+        if (!prevPoint) return;
+        e = prevPoint;
+    }
+    const x = e.clientX ?? e.touches?.[0].clientX ?? e.x;
+    const y = e.clientY ?? e.touches?.[0].clientY ?? e.y;
+    const radius = uiCanvas.classList.contains("eraser") ? Number(eraserSize.value) : Number(penSize.value);
+    const { width, height } = uiCanvas;
+    uiCtx.clearRect(0, 0, width, height);
+    uiCtx.fillStyle = uiColor;
+    uiCtx.beginPath();
+    uiCtx.arc(x, y, radius, 0, Math.PI*2);
+    uiCtx.fill();
+    if (drawing) draw(x, y);
+    prevPoint = {x, y};
+}
+
+//nav buttons
+eraseButton.addEventListener("click", () => {
+    const { width, height } = drawingCanvas;
+    drawingCtx.clearRect(0, 0, width, height);
+});
+downloadButton.addEventListener("click", () => {
+    const link = document.createElement("a");
+    link.download = "drawing.png";
+    link.href = drawingCanvas.toDataURL();
+    link.click();
+});
 
 //color changing
 document.querySelectorAll("[id|='color']").forEach(b => {
@@ -41,7 +85,8 @@ window.addEventListener("keyup", handleKeyup);
 function handleKeyup({ key }) {
     const k = key.toLowerCase();
     if (k === "x") { //swap tools
-        return document.querySelector("[type='radio']:not(:checked)").click();
+        document.querySelector("[type='radio']:not(:checked)").click();
+        return moveUiCursor();
     }
     if (["=", "+"].includes(k)) {
         const eraserActive = uiCanvas.classList.contains("eraser");
@@ -60,4 +105,5 @@ function handleKeyup({ key }) {
 
 function changeToolSize(tool, deltaValue) {
     tool.value = Math.max(1, Number(tool.value) + deltaValue);
+    moveUiCursor();
 }
