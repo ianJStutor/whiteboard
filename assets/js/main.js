874de6138f0ffd7b1117917cc566bc83b2488134
Ian Marshall <ian.js.tutor@gmail.com>
2022-08-20 07:12:52 -0400

    custom cursor and other minor ui adjustments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,23 @@
 //DOM elements
+const [drawingCanvas, uiCanvas] = document.querySelectorAll("canvas");
 const header = document.querySelector("header");
+const nav = document.querySelector("nav");
 const [color, penSize, eraserSize] = document.querySelectorAll("fieldset > input");
+
+//canvases
+const drawingCtx = drawingCanvas.getContext("2d");
+const uiCtx = uiCanvas.getContext("2d");
+setCanvasSize();
+function setCanvasSize() {
+    drawingCanvas.width = uiCanvas.width = window.innerWidth;
+    drawingCanvas.height = uiCanvas.height = window.innerHeight - nav.clientHeight;
+}
+window.addEventListener("resize", setCanvasSize);
+
+//cursor
+document.querySelectorAll("[name='tool']").forEach(t => {
+    t.addEventListener("change", () => canvas.classList.toggle("eraser"));
+});
 
 //remove header
 document.addEventListener("click", () => header.classList.add("hide"), {once: true});
